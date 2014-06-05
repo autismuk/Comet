aa32927391f8bf7d0caa6373818661af35506866
Paul Robson <paul@robsons.org.uk>
2014-06-05 14:18:39 +0100

    Updated Doc, Bouncing crab demo

--- system/comet.lua
+++ system/comet.lua
@@ -555,55 +555,9 @@ function AbstractSystemClass:update(entityList)
 	error("AbstractSystemClass is abstract and must be subclassed")
 end 
 
---- ************************************************************************************************************************************************************************
---- ************************************************************************************************************************************************************************
+return Comet 
 
-local cm = Comet:new() 
-c0 = cm:newC("position",{ x = 100,y = 100 })
-c2 = cm:newC("size", {width = 80,height = 70})
-c3 = cm:newC("velocity", { dx = 32, dy = 0 })
-c4 = cm:newC("rotation", { rotation = 45 })
-c5 = cm:newC("rotatespeed", { da = 360 })
-
-c1 = cm:newC("sprite", { handle = nil, requires = { c0,c2 },
-						 constructor = function(e) e.handle = display.newImage("images/crab.png") end,
-						 destructor = function(e) e.handle:removeSelf() end,
-})
-
-local s1 = cm:newS({c0,c1},function (e) e.handle.x,e.handle.y = e.x,e.y end, { preProcess = function(el) end })
-local s2 = cm:newS({c1,c2},function (e) e.handle.width,e.handle.height = e.width,e.height end)
-
-local s4 = cm:newS({c1,c4},function (e) e.handle.rotation = e.rotation end)
-
-ClassS5 = cm:getSystemClass():new()
-function ClassS5:update(entityList)
-	local dt = entityList[1]:getInfo().deltaTime
-	for i = 1,#entityList do 
-		local e = entityList[i]
-		e.rotation = e.rotation + e.da * dt
-	end 
-end 
-
-ClassS5:new(cm,{c4,c5})
-
-local s3 = cm:newS({c0,c3},function (e)
-	local dt = e:getInfo().deltaTime
-	e.x = e.x + e.dx * dt
-	e.y = e.y + e.dy * dt
-	if e.x < 0 or e.y < 0 or e.x > 320 or e.y > 480 then e.dx = -e.dx e.dy = -e.dy end
-end)
-
-local e1 = cm:newE({c1,c3},{ x = 0,y = 0,dx = 32,dy = 48 }) e1.__name = "e1"
-e1:addC("rotation,rotatespeed")
-print(s1.query:toString())
-cm:runAutomatic()
-
---_G.Comet = Comet  require("bully")
-
--- TODO Move to a library
--- TODO First working test
 -- TODO require("controller")
 -- TODO Make it the main branch.
 -- TODO Creating entities from a JSON.
 -- TODO Timer Components
-
